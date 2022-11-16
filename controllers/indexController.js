10d7f256cd57e19adcbffcd85256ea52fe328387
GiaPhat <phatng.it.dev@gmail.com>
2022-11-17 01:30:24 +0700

    fix: UI template and display data

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -2,11 +2,10 @@ const express = require("express");
 const fetch = require("node-fetch");
 
 let getData = async (req, res) => {
-  const API = "http://api.weatherapi.com/v1/current.json?key=a2c4ce37a28b431bb1292413221411&q=" + req.body.location;
+  const API = "http://api.weatherapi.com/v1/current.json?key=fd85542d4d33427895e174003221611&q=" + req.body.location;
   const response = await fetch(API);
   const data = await response.json();
-  return res.status(200).json({data: data});
-
+  return res.status(200).json({ data: data });
 };
 
 let data = (req, res) => {
