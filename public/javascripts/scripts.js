6a18b8267eb31ced51dcbb922e1831687b902d65
GiaPhat <phatng.it.dev@gmail.com>
2022-11-17 03:15:02 +0700

    fix: edit static file

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -1,25 +1,39 @@
-const form = document.getElementById("form");
-const location = document.getElementById("location");
-const url = form.getAttribute("action");
+function myFunction() {
+  const location = document.getElementById("location").value;
+  var myHeaders = new Headers();
+  myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
 
-form.addEventListener("submit", function (event) {
-  event.preventDefault();
-  const location = location.value;
-  const data = { location: location };
+  var urlencoded = new URLSearchParams();
+  urlencoded.append("token", "az02n1249clasfhxaroqw");
+  urlencoded.append("time", "now");
+  urlencoded.append("type", "all");
+  urlencoded.append("location", location);
 
-  fetch(url, {
-    method: "GET",
-    headers: {
-      "Content-Type": "application/json",
-      Accept: "application/json",
-    },
-    body: JSON.stringify(data),
-  })
-    .then((response) => response.json())
-    .then((data) => {
-      console.log("Success:", data);
+  var requestOptions = {
+    method: "POST",
+    headers: myHeaders,
+    body: urlencoded,
+    redirect: "follow",
+  };
+
+  fetch("http://localhost:3000", requestOptions)
+    .then((response) => response.text())
+    .then((result) => {
+      const fRe = JSON.parse(result);
+      //console.log(result.data.location.name)
+      document.getElementById("name").innerHTML = fRe.data.location.name;
+      document.getElementById("day").innerHTML = fRe.data.location.localtime;
+      document.getElementById("status").innerHTML = fRe.data.current.condition.text;
+      document.getElementById("temp").innerHTML = fRe.data.current.temp_c + `&#176;`;
+
+      document.getElementById("cloudy").innerHTML = fRe.data.current.cloud + "%";
+      document.getElementById("hum").innerHTML = fRe.data.current.humidity + "%";
+      document.getElementById("win").innerHTML = fRe.data.current.wind_dir;
+      document.getElementById("rai").innerHTML = fRe.data.location.localtime_epoch;
+      document.getElementById("tempf").innerHTML = fRe.data.current.temp_f + `&#176;` + "F";
+      document.getElementById("tempc").innerHTML = fRe.data.current.temp_c + `&#176;` + "C";
+
+      document.getElementById("icon").src = fRe.data.current.condition.icon;
     })
-    .catch((error) => {
-      console.error("Error:", error);
-    });
-});
+    .catch((error) => console.log("error", error));
+}
